4aa51cf88772efb9a46b9c6a5ebcbc1126dbb9a7
Bill Lowe <bill.lowe87@gmail.com>
2026-01-29 13:09:22 +0000

    404 page, layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,14 @@ const btn = document.getElementById('mobile-menu-btn');
 const menu = document.getElementById('mobile-menu');
 
 btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+	menu.classList.toggle('hidden');
 });
 
 // Close mobile menu on link click
 menu.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.add('hidden');
-    });
+	link.addEventListener('click', () => {
+		menu.classList.add('hidden');
+	});
 });
+
+
